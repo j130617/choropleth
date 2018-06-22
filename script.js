24fe817293731cd7c564570a4b1a9c1bb708a7ca
j130617 <39986473+j130617@users.noreply.github.com>
2018-06-22 10:27:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -86,8 +86,8 @@ info.onAdd = function (map) {
 
 // Edit info box text and variables (such as props.density2010) to match those in your GeoJSON data
 info.update = function (props) {
-  this._div.innerHTML = '<h4>Inwoners uit MOE-landen per postcodegebied</h4>' +  (props ?
-    '<b>' + props.PC4CODE + '</b><br />' + props.density + ' inwoners uit MOE-landen'
+  this._div.innerHTML = '<h4>Aantal inwoners uit MOE-landen</h4>' +  (props ?
+    '<b>' 'Postcodegebied' + props.PC4CODE + '</b><br />' + props.density + ' inwoners uit MOE-landen'
     : 'Beweeg met de muis over de kaart');
 };
 info.addTo(map);
